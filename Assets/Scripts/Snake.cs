72039ec9dd071d7b5cb9390daa0b672072371e64
Abhaymodi860 <modiabhay860@gmail.com>
2024-09-04 22:17:05 +0530

    snake growth added with death

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments = new List<Transform>();
+    public Transform segementPrefab;
+    public int initialSize = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //_segments = new List<Transform>();
+        //_segments.Add(this.transform);
+        ResetState();
     }
 
     // Update is called once per frame
@@ -33,9 +38,51 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = _segments.Count - 1; i > 0; i--) 
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x, 
             Mathf.Round(this.transform.position.y) + _direction.y, 
             0.0f);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segementPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);  
+
+        for (int i = 1; i < this.initialSize; i++) 
+        {
+            _segments.Add(Instantiate(segementPrefab));
+        }
+
+        this.transform.position = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+        {
+            Grow();
+        } else if (collision.CompareTag("Obstacle"))
+        {
+            ResetState();
+        }
     }
 }
